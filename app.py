27209c14db96b6186dd6a0a0033a4d3722b86efd
samip1990 <samip1990@gmail.com>
2020-05-06 00:44:24 -0500

    maintenance service added

--- app.py
+++ app.py
@@ -9,7 +9,28 @@ app = Flask(__name__)
 
 PORT = os.getenv("PORT")
 
-paymentHistories = []
+payment_histories = []
+maintenance_requests = []
+
+# ----------------------------- Maintenance Request  -----------------------------
+
+@app.route("/maintenance-request", methods=["POST"])
+def maintenance_request():
+    body = request.json
+    body["requestId"] = str(uuid.uuid1())
+
+    maintenance_requests.append(body)
+    print(type(body))
+    return json.dumps({"result": "success", "body": body})
+
+@app.route("/maintenance-request/<id>", methods=["GET"])
+def get_maintenance_request(id):
+    x = maintenance_requests
+    print(x)
+    return json.dumps(maintenance_requests[0])
+
+# ----------------------------- Payment Service      -----------------------------
+
 
 @app.route("/payment-processing", methods=["POST"])
 def process_payment():
@@ -20,13 +41,12 @@ def process_payment():
 
     payment_response = PaymentResponse(body['paymentConfirmationNumber'], body['sucessFlag'])
 
-    paymentHistories.append(body)
-    print (paymentHistories[0])
+    payment_histories.append(body)
     return (json.dumps(payment_response.__dict__))
 
 @app.route("/payment-processing/<id>", methods=["GET"])
 def get_payment_record(id):
-    return jsonify(paymentHistories[0])
+    return jsonify(payment_histories[0])
 
 if __name__ == "__main__":
     app.run(debug=False, port=9998, host="localhost")
--- client.py
+++ client.py
@@ -1,7 +1,29 @@
 import requests
 import json
 from PaymentInformation import PaymentInformation
+from MaintenanceRequest import MaintenanceRequest
 
+
+
+# ----------------------------- Maintenance Request  -----------------------------
+
+maintenanceRequest = MaintenanceRequest("PLUMBING", "05-May-2020", "03-May-2020",
+                                        "plumbing issue in master bathroom",
+                                        "john1980", "NOT STARTED")
+
+r = requests.post("http://localhost:9998/maintenance-request",
+                  headers={"Content-Type":"application/json"},
+                  data=json.dumps(maintenanceRequest.__dict__))
+
+maintenance = r.json()
+print(r.json())
+
+r = requests.get("http://localhost:9998/maintenance-request/"+maintenance["body"]["userId"],
+                  headers={"Content-Type":"application/json"})
+print(r.json())
+
+
+# ----------------------------- Payment Service      -----------------------------
 
 paymentInfo = PaymentInformation("VISA", "John", "2456098745674326", "345", 1300.00, "john1980")
 
